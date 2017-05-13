ed70c9179cbc3d14ccb973c19b8f3402baa3412a
Oguz Erdi Erkmen <erdi.erkmen@gmail.com>
2017-05-13 12:12:27 +0300

    Second camera added

--- OpenCVCalisma/main.cpp
+++ OpenCVCalisma/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <time.h>
 #include <iomanip>
+#include <pthread.h>
 
 #define IMAGE_SIZE40_40 Size(40,40)
 #define IMAGE_SIZE64_64 Size(64,64)
@@ -35,6 +36,7 @@ int main() {
 void videoCapturing()
 {
     Mat image;
+    Mat imageCable;
     Mat outImage;
     char key = 'e';
     time_t start, end;
@@ -58,6 +60,10 @@ void videoCapturing()
         exit(0);
     }
 
+    VideoCapture captureCable(2);
+    if (!captureCable.isOpened()) {
+        cout << "R doesn't work" << endl;
+    }
 
     while(key != 'q'){
         key = waitKey(25);
@@ -70,153 +76,157 @@ void videoCapturing()
 
         imshow("IMAGE",image);
 
-        time(&end);
-        counter++;
-
-        sec = difftime(end, start);
-        fps = counter / sec;
-
-        if (counter == (INT_MAX - 1000))
-            counter = 0;
-
-        ostringstream convert;
-        if(counter>30) {
-            convert << fixed << setprecision(3) << fps;
-        }
-
-        Mat output;
-        image.copyTo(output);
-
-        String fpsRes = "FPS : " + convert.str();
-        putText(output, fpsRes , cvPoint(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0,0,255), 1);
-        // imshow("image : ",image);
-
-        image.copyTo(outImage);
-        cvtColor(image, image, COLOR_BGR2GRAY);
-        GaussianBlur(image, imageGoussOut, Size(7,7), 1.5, 1.5);
-        //imshow("Gaus Result",imageGoussOut);
-        Mat cannyOut;
-        Canny(imageGoussOut, cannyOut, 0, 30, 3);
-
-        //imshow("Canny Result",cannyOut);
-        GaussianBlur(cannyOut, cannyOut, Size(7,7), 1.5, 1.5);
-
-        //TODO : Change this to Morphology
-        threshold(cannyOut,cannyOut,0,255,THRESH_BINARY);
+        captureCable>>imageCable;
+        imshow("IMAGECAble",imageCable);
 
 
-
-        imshow("GausCanny Result",cannyOut);
-        findContours(cannyOut, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-        double maxArea = 0;
-        for(int j = 0 ; j< contours.size() ; ++j){
-            if(maxArea < contourArea(contours[j])){
-                maxArea = contourArea(contours[j]);
-            }
-        }
-
-        for (int i = 0; i < contours.size(); i++) {
-            Scalar color = Scalar(0, 0, 255);
-            if (maxArea == contourArea(contours[i])) {
-                // drawContours(output, contours, i, color, 2, 8, hierarchy, 0, Point());
-                double area = contourArea(contours[i]);
-                RotatedRect boundRect;
-                boundRect = minAreaRect(contours[i]);
-
-
-                //displayFotoNums(output,"Area : " ,area,Point(20,120));
-                if (area > 4000) {
-                    Point2f vertices[4];
-                    boundRect.points(vertices);
-                    for (int i = 0; i < 4; i++)
-                        line(output, vertices[i], vertices[(i + 1) % 4], Scalar(0, 255, 0), 2, 8, 0);
-
-                    //gövdeyi çiz
-
-
-                    //rectangle(output, boundRect, Scalar(0, 255, 0), 2, 8, 0);
-
-                    // imshow("Contours : ", output);
-                    stringstream ss;
-                    ss << imageNumPos;
-                    string name = "CinaliPos40_40\\" + ss.str() + ".png";
-                    ++imageNumPos;
-
-
-                    vector<Vec3f> circles;
-                    /// Apply the Hough Transform to find the circles
-                    HoughCircles(imageGoussOut, circles, CV_HOUGH_GRADIENT, 1, imageGoussOut.rows / 8, 75, 30, 0, 0);
-
-                    Point centerM;
-                    int radiusM = 0;
-
-                    if (circles.size() != 0) {
-                        for (size_t i = 0; i < circles.size(); i++) {
-                            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-
-
-                            centerM.x += center.x;
-                            centerM.y += center.y;
-                            radiusM += cvRound(circles[i][2]);
-
-
-                        }
-
-                        centerM.x = centerM.x / circles.size();
-                        centerM.y = centerM.y / circles.size();
-
-                        radiusM = radiusM / circles.size();
-
-                        //center of circle
-                        circle(output, centerM, 3, Scalar(0, 255, 255), -1, 8, 0);
-                        // circle drawing for head
-                        circle(output, centerM, radiusM, Scalar(0, 255, 255), 3, 8, 0);
-
-                        // imshow("Head : ", output);
-
-                    }
-                    displayFotoNums(output, "Center X: ", centerM.y, Point(15, 80));
-                    displayFotoNums(output, "Center Y: ", centerM.x, Point(15, 100));
-                    displayFotoNums(output, "Radius: ", radiusM, Point(15, 120));
-                    displayFotoNums(output, "CenterRectX: ", boundRect.center.y, Point(15, 160));
-                    displayFotoNums(output, "CenterRectY: ", boundRect.center.x, Point(15, 180));
-
-                    circle(output, boundRect.center, 3, Scalar(255, 0, 255), -1, 8, 0);
-
-                    //draw the line to head and body
-                    // line(output, boundRect.center, centerM, Scalar(255,0,0));
-
-
-
-                    Point temp(0, 0);
-
-
-
-                    temp.x = (int) (boundRect.center.x -
-                                    (centerM.x - boundRect.center.x));
-                    temp.y = (int) (boundRect.center.y - (centerM.y - boundRect.center.y));
-
-                    if(circles.size() != 0){
-                        line(output, temp, boundRect.center, Scalar(255, 0, 0), 2, 8, 0);
-                    }
-
-                    // displayFotoNums(output,"Foto Pos : " ,imageNumPos,Point(15,60));
-                    // putText(image, imageNumPos , cvPoint(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0,0,255), 1);
-                    //generateHistogram(output);
-
-                    displayFotoNums(output, "New Point X: ", temp.y, Point(15, 200));
-                    displayFotoNums(output, "New Point Y: ", temp.x, Point(15, 220));
-
-                    circle(output, temp, 3, Scalar(255, 0, 255), -1, 8, 0);
-                    // imshow("Body Line ", output);
-
-                }
-
-            }
-
-        }
-        imshow("imageCont : ", output);
+//        time(&end);
+//        counter++;
+//
+//        sec = difftime(end, start);
+//        fps = counter / sec;
+//
+//        if (counter == (INT_MAX - 1000))
+//            counter = 0;
+//
+//        ostringstream convert;
+//        if(counter>30) {
+//            convert << fixed << setprecision(3) << fps;
+//        }
+//
+//        Mat output;
+//        image.copyTo(output);
+//
+//        String fpsRes = "FPS : " + convert.str();
+//        putText(output, fpsRes , cvPoint(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0,0,255), 1);
+//        // imshow("image : ",image);
+//
+//        image.copyTo(outImage);
+//        cvtColor(image, image, COLOR_BGR2GRAY);
+//        GaussianBlur(image, imageGoussOut, Size(7,7), 1.5, 1.5);
+//        //imshow("Gaus Result",imageGoussOut);
+//        Mat cannyOut;
+//        Canny(imageGoussOut, cannyOut, 0, 30, 3);
+//
+//        //imshow("Canny Result",cannyOut);
+//        GaussianBlur(cannyOut, cannyOut, Size(7,7), 1.5, 1.5);
+//
+//        //TODO : Change this to Morphology
+//        threshold(cannyOut,cannyOut,0,255,THRESH_BINARY);
+//
+//
+//
+//        imshow("GausCanny Result",cannyOut);
+//        findContours(cannyOut, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+//
+//        double maxArea = 0;
+//        for(int j = 0 ; j< contours.size() ; ++j){
+//            if(maxArea < contourArea(contours[j])){
+//                maxArea = contourArea(contours[j]);
+//            }
+//        }
+//
+//        for (int i = 0; i < contours.size(); i++) {
+//            Scalar color = Scalar(0, 0, 255);
+//            if (maxArea == contourArea(contours[i])) {
+//                // drawContours(output, contours, i, color, 2, 8, hierarchy, 0, Point());
+//                double area = contourArea(contours[i]);
+//                RotatedRect boundRect;
+//                boundRect = minAreaRect(contours[i]);
+//
+//
+//                //displayFotoNums(output,"Area : " ,area,Point(20,120));
+//                if (area > 4000) {
+//                    Point2f vertices[4];
+//                    boundRect.points(vertices);
+//                    for (int i = 0; i < 4; i++)
+//                        line(output, vertices[i], vertices[(i + 1) % 4], Scalar(0, 255, 0), 2, 8, 0);
+//
+//                    //gövdeyi çiz
+//
+//
+//                    //rectangle(output, boundRect, Scalar(0, 255, 0), 2, 8, 0);
+//
+//                    // imshow("Contours : ", output);
+//                    stringstream ss;
+//                    ss << imageNumPos;
+//                    string name = "CinaliPos40_40\\" + ss.str() + ".png";
+//                    ++imageNumPos;
+//
+//
+//                    vector<Vec3f> circles;
+//                    /// Apply the Hough Transform to find the circles
+//                    HoughCircles(imageGoussOut, circles, CV_HOUGH_GRADIENT, 1, imageGoussOut.rows / 8, 75, 30, 0, 0);
+//
+//                    Point centerM;
+//                    int radiusM = 0;
+//
+//                    if (circles.size() != 0) {
+//                        for (size_t i = 0; i < circles.size(); i++) {
+//                            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+//
+//
+//                            centerM.x += center.x;
+//                            centerM.y += center.y;
+//                            radiusM += cvRound(circles[i][2]);
+//
+//
+//                        }
+//
+//                        centerM.x = centerM.x / circles.size();
+//                        centerM.y = centerM.y / circles.size();
+//
+//                        radiusM = radiusM / circles.size();
+//
+//                        //center of circle
+//                        circle(output, centerM, 3, Scalar(0, 255, 255), -1, 8, 0);
+//                        // circle drawing for head
+//                        circle(output, centerM, radiusM, Scalar(0, 255, 255), 3, 8, 0);
+//
+//                        // imshow("Head : ", output);
+//
+//                    }
+//                    displayFotoNums(output, "Center X: ", centerM.y, Point(15, 80));
+//                    displayFotoNums(output, "Center Y: ", centerM.x, Point(15, 100));
+//                    displayFotoNums(output, "Radius: ", radiusM, Point(15, 120));
+//                    displayFotoNums(output, "CenterRectX: ", boundRect.center.y, Point(15, 160));
+//                    displayFotoNums(output, "CenterRectY: ", boundRect.center.x, Point(15, 180));
+//
+//                    circle(output, boundRect.center, 3, Scalar(255, 0, 255), -1, 8, 0);
+//
+//                    //draw the line to head and body
+//                    // line(output, boundRect.center, centerM, Scalar(255,0,0));
+//
+//
+//
+//                    Point temp(0, 0);
+//
+//
+//
+//                    temp.x = (int) (boundRect.center.x -
+//                                    (centerM.x - boundRect.center.x));
+//                    temp.y = (int) (boundRect.center.y - (centerM.y - boundRect.center.y));
+//
+//                    if(circles.size() != 0){
+//                        line(output, temp, boundRect.center, Scalar(255, 0, 0), 2, 8, 0);
+//                    }
+//
+//                    // displayFotoNums(output,"Foto Pos : " ,imageNumPos,Point(15,60));
+//                    // putText(image, imageNumPos , cvPoint(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0,0,255), 1);
+//                    //generateHistogram(output);
+//
+//                    displayFotoNums(output, "New Point X: ", temp.y, Point(15, 200));
+//                    displayFotoNums(output, "New Point Y: ", temp.x, Point(15, 220));
+//
+//                    circle(output, temp, 3, Scalar(255, 0, 255), -1, 8, 0);
+//                    // imshow("Body Line ", output);
+//
+//                }
+//
+//            }
+//
+//        }
+//        imshow("imageCont : ", output);
     }
 }
 
@@ -312,5 +322,10 @@ void generateHistogram(Mat image)
     /// Display
     //namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
     imshow("Histogram", histImage );
+
+}
+
+void * secondCam(void)
+{
 
 }
